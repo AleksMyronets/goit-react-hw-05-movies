f4bd703d0edf75e136dede980d698d4c52562171
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-05-30 17:16:16 +0300

    Update Movie.jsx

---
+++ src/components/Movie/Movie.jsx
@@ -0,0 +1,32 @@
+import { Link, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import css from './Movie.module.css';
+
+const Movie = ({ films }) => {
+  const locatioRender = useLocation();
+  return (
+    <>
+      {
+        <ul>
+          {films.map(({ title, id }) => (
+            <li key={id}>
+              <Link to={`${id}`} state={locatioRender} className={css.HomeLink}>
+                {title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      }
+    </>
+  );
+};
+export default Movie;
+
+Movie.propTypes = {
+  films: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+    })
+  ),
+};
