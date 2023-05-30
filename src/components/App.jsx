a0968c7b535c87cefec983e6da3e5fd9a67fa513
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-05-30 14:09:06 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,26 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { lazy } from 'react';
+import Layout from './Layout/Layout'
+const Home = lazy(() => import('./Pages/Home'));
+const Movies = lazy(() => import('./Pages/Movies'));
+const MovieDetails = lazy(() => import('./Pages/MovieDetails'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+
 export const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="/movies/:movieId/cast" element={<Cast />} />
+            <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
